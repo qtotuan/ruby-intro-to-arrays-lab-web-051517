ee6996fcc5d18b62a109d50815052a53c56f6500
Quynh To Tuan <quynh.totuan@gmail.com>
2017-04-25 23:52:15 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  Array.new
+end
+
+def array_with_two_elements
+  ["a", "b"]
+end
+
+def first_element(arr)
+  arr[0]
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  arr[-1]
+end
+
+def first_element_with_array_methods(array)
+	array.first
+end
+
+def last_element_with_array_methods(array)
+	array.last
+end
+
+def length_of_array(array)
+	array.size
+end
